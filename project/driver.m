63a1e90a764ee7c12bab5b60f453179bd57d4ed3
WuHongli2131 <133666499+WuHongli2131@users.noreply.github.com>
2025-01-08 12:40:12 +0800

    大致思路理清，有些细节需要确认
    下午就能确认完，但还差网格和KF阵

--- project/driver.m
+++ project/driver.m
@@ -1,21 +1,35 @@
 %2D
 clear all; clc;
-
-kappa = 1.0; % conductivity
-
+%%整体思路：利用一个逻辑值来控制当前反向，然后在当前方向视为一个自由度二维问题，解出来后循环到下一个自由度，最后拼起来即可（当前版本不考虑3维）
+%%参数导入   需要用户修改的部分
+kappa = 1.0; % conductivity 修改为mu和lamda
+mu=1;
+lamda=1;%具体数值需要修改对比
+D = zeros(3);%D阵一步到胃
+D(1,1)=2*mu+lamda;
+D(2,2)=D(1,1);
+D(1,2)=lamda;
+D(2,1)=D(1,2);
+D(3,3)=mu;
+%%检验方程
 % exact solution
-exact = @(x,y) x*(1-x)*y*(1-y);
-exact_x = @(x,y) (1-2*x)*y*(1-y);
-exact_y = @(x,y) x*(1-x)*(1-2*y);
+dir=1;
+v_x=0;%没算出来 先确定第一个方向再说
+v_y=0;
+exact = @(x,y) (dir==1)*x*(1-x)*y*(1-y)+(dir==2)*x*(1-x)*y*(1-y);%dir表示当前方向，在前方加入循环即可将代码实现多次计算
+exact_x = @(x,y) (dir==1)*(1-2*x)*y*(1-y)+(dir==2)*v_x;
+exact_y = @(x,y) (dir==1)*x*(1-x)*(1-2*y)+(dir==2)*v_y;
 
 f = @(x,y) 2.0*kappa*x*(1-x) + 2.0*kappa*y*(1-y); % source term
 
+%%四边形quadrature rule部分
 % quadrature rule
-n_int_xi  = 3;
-n_int_eta = 3;
+n_int_xi  = 10;
+n_int_eta = 10;
 n_int     = n_int_xi * n_int_eta;
 [xi, eta, weight] = Gauss2D(n_int_xi, n_int_eta);%尝试修改 GAUSS 切换后生成高斯网格
 
+%%生成网格部分，似乎已经被gmsh替代
 % mesh generation
 n_en   = 4;               % number of nodes in an element
 n_el_x = 6;               % number of elements in x-dir 划分单元格
@@ -29,8 +43,9 @@ n_np   = n_np_x * n_np_y; % total number of nodal points 总节点数
 x_coor = zeros(n_np, 1);  %坐标 两者相同   划分为三角形时 每个矩形被划分为两个三角形 于是 n_el翻倍 
 y_coor = x_coor;            %但是n_np不变
 
-hx = 1.0 / n_el_x;        % mesh size in x-dir  在三角形中hx需要*2
+hx = 1.0 / n_el_x;        % mesh size in x-dir  在三角形中hx需要*2  网格不均匀？
 hy = 1.0 / n_el_y;        % mesh size in y-dir
+
 
 % generate the nodal coordinates
 for ny = 1 : n_np_y
@@ -41,7 +56,9 @@ for ny = 1 : n_np_y
   end
 end
 
+%%IEN部分 这里可以加一个判断 跳过重新生成Ien
 % IEN array
+if dir==1  %仅在第一次生成IEN，否则后续的IEN会覆盖掉前面的IEN
 IEN = zeros(n_el, n_en); %element和其所连接的点的关系
 for ex = 1 : n_el_x
   for ey = 1 : n_el_y
@@ -67,27 +84,46 @@ end
 n_eq = counter;%计算内部网格
 
 LM = ID(IEN);%略显抽象，只知道结果是把方程映射到了节点上
+else
+end
 
+
+%%k阵和f阵建立 
 % allocate the stiffness matrix and load vector
 K = spalloc(n_eq, n_eq, 9 * n_eq);%建立空阵 9还是很抽象  不用改
 F = zeros(n_eq, 1);
+
+%在这里理清一下思路 使用BDB矩阵表示K
+%需要B阵，并且B阵与a和b无关
+%需要向量e，不与a和b有关 和方向有关 脑子晕了这里   与i和j有关
+%于是在一个单元内，定义出BDB 然后再与eiej循环？ 但是和Kele的关系？
+%需要D阵，已定义
 
 % loop over element to assembly the matrix and vector
 for ee = 1 : n_el
   x_ele = x_coor( IEN(ee, 1:n_en) );
   y_ele = y_coor( IEN(ee, 1:n_en) );
   
-  k_ele = zeros(n_en, n_en); % element stiffness matrix
-  f_ele = zeros(n_en, 1);    % element load vector
+  k_ele = zeros(n_en, n_en); % element stiffness matrix 最后应该是一个三对角
+  f_ele = zeros(n_en, 1);    % elemenlt oad vector
   
   for ll = 1 : n_int
     x_l = 0.0; y_l = 0.0;
     dx_dxi = 0.0; dx_deta = 0.0;%xi表示可惜 eta表示伊塔 用于处理链式法则
     dy_dxi = 0.0; dy_deta = 0.0;
+    Blb=zeros(3,2);%blackboard是一个3*2的矩阵 也就是B阵
+    for aa = 1 : n_en
+      [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));%一阶导拟合 B阵在这里完成 
+      Blb(1,1)=Blb(1,1)+Na_xi;
+      Blb(2,2)=Blb(2,2)+Na_eta;
+      Blb(3,2)=Blb(1,1);
+      Blb(3,1)=Blb(2,2);%单元内B阵完成
+    end
+    
     for aa = 1 : n_en
       x_l = x_l + x_ele(aa) * Quad(aa, xi(ll), eta(ll)); %取单个节点进行拟合
       y_l = y_l + y_ele(aa) * Quad(aa, xi(ll), eta(ll));    
-      [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));%一阶导拟合
+      [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));%一阶导拟合  
       dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;%单节点的导数一起处理了
       dx_deta = dx_deta + x_ele(aa) * Na_eta;
       dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
@@ -102,7 +138,7 @@ for ee = 1 : n_el
       Na_x = (Na_xi * dy_deta - Na_eta * dy_dxi) / detJ;
       Na_y = (-Na_xi * dx_deta + Na_eta * dx_dxi) / detJ;
       
-      f_ele(aa) = f_ele(aa) + weight(ll) * detJ * f(x_l, y_l) * Na;
+      f_ele(aa) = f_ele(aa) + weight(ll) * detJ * f(x_l, y_l) * Na;%需要修改 似乎不用，i和j表示方向后就是看dir 但是两个方向是分开的 只需要最后存储的时候注意就可以了
       
       for bb = 1 : n_en
         Nb = Quad(bb, xi(ll), eta(ll));
@@ -110,7 +146,7 @@ for ee = 1 : n_el
         Nb_x = (Nb_xi * dy_deta - Nb_eta * dy_dxi) / detJ;
         Nb_y = (-Nb_xi * dx_deta + Nb_eta * dx_dxi) / detJ;
         
-        k_ele(aa, bb) = k_ele(aa,bb) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y);
+        k_ele(aa, bb) = k_ele(aa,bb) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y);%需要修改
       end % end of bb loop
     end % end of aa loop
   end % end of quadrature loop
